8735ef63414f32374eeb7c66c9749d189cc89c77
osogrizz <leo.htor@gmail.com>
2021-01-21 13:56:08 -0800

    Adds contentful api and dotenv dependencies.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,16 @@
+require("dotenv").config({
+    path: `.env.${process.env.NODE_ENV}`,
+})
+
 module.exports = {
     plugins: [
-
+        {
+            resolve: `gatsby-source-contentful`,
+            options: {
+                spaceId: process.env.SPACE_ID,
+                // Learn about environment variables: https://gatsby.dev/env-vars
+                accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+            },
+        },
     ]
 }
